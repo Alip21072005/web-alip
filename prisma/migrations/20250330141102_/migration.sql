19091cac874a76a21ac4a5c65c2eb17f407d0b3a
Alip21072005 <muhamadalipmaulana3@gmail.com>
2025-03-30 21:12:26 +0700

    Refactor database schema: add createdAt and updatedAt fields to User model; recreate migration files

--- prisma/migrations/20250330141102_/migration.sql
+++ prisma/migrations/20250330141102_/migration.sql
@@ -16,6 +16,8 @@ CREATE TABLE "User" (
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
